4c13d3471818ecf735abdd44ee5656693417e92e
Premkr1234 <premkr.315@gmail.com>
2023-04-02 18:43:49 +0530

    hellthtj6

--- src/org/account/Login.java
+++ src/org/account/Login.java
@@ -4,6 +4,10 @@ public class Login extends SignUp {
 	public void btnLogin() {
 		System.out.println("BtnLogin");
 	}
+	public void mob() {
+	System.out.println("tam");
+
+	}
 	public static void main(String[] args) {
 		Login l =new Login();
 		l.userName();
